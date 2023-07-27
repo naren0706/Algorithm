a389f6c60f68dc7248497cf4717d655ffcef64a2
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-27 11:36:12 +0530

    [Narenthra Kishore] Add : new solution Binary Search

---
+++ Algorithm/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class BinarySearch
+    {
+        public void ReadAllText(string filepath)
+        {
+            string allText = File.ReadAllText(filepath);
+            string[] words = allText.Split(",");
+            Console.WriteLine("Enter the key to search");
+            string key = Console.ReadLine();
+            bool flag = false;
+            foreach (string word in words)
+            {
+                if (word.Equals(key))
+                {
+                    flag = true;
+                }
+            }
+            if(!flag)
+                Console.WriteLine("not found");
+            else
+                Console.WriteLine("Search element {0} found",key);
+        }
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static string searchFilePath = @"E:\BridgeGateProblems\Algorithm\Algorithm\Files\Search.txt";
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.binary Search\n2.Anagram");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        BinarySearch binarySearch = new BinarySearch();
+                        binarySearch.ReadAllText(searchFilePath);
+                        break;
+                }
+            }
+        }
+    }
+}
