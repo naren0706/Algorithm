504e4b481092cde1fd049814ec948cb69cc82610
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-27 12:07:42 +0530

    [Narenthra Kishore] Add : new solution for String ReplaceMent

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -10,13 +10,24 @@ namespace Algorithm
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.binary Search\n2.Anagram");
+                Console.WriteLine("1.binary Search\n2.Anagram\n3.String Replace");
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
                     case 1:
                         BinarySearch binarySearch = new BinarySearch();
                         binarySearch.ReadAllText(searchFilePath);
+                        break;
+                    case 2:
+                        Anagrams anagrams = new Anagrams();
+                        anagrams.IsAnagram();
+                        break;
+                    case 3:
+                        StringReplace stringReplace = new StringReplace();
+                        stringReplace.ReplaceString();
+                        break;
+                    default:
+                        flag = false;
                         break;
                 }
             }
---
+++ Algorithm/StringReplace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class StringReplace
+    {
+        public void ReplaceString()
+        {
+            string line = "Hello <<name>>, We have your fullname as << full name >> in our system. your contact number is 91 - xxxxxxxxxx. " +
+                "Please,let us know in case of any clarification Thank you BridgeLabz 01 / 01 / 2016.";
+            Console.WriteLine("Enter Your Full name :");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Enter Your Short name :");
+            string shorName = Console.ReadLine();
+            Console.WriteLine("Enter Your PhoneNumber :");
+            string number = Console.ReadLine();
+            line = line.Replace("<<name>>", shorName);
+            line = line.Replace("<< full name >>", fullName);
+            line = line.Replace("xxxxxxxxxx", number);
+            Console.WriteLine("Final Result :" + line);
+        }
+    }
+}
